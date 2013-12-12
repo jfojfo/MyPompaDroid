531bc5b35e60b7355a3d5b42dd075cfb4496ea3b
jfojfo <jfojfo@gmail.com>
2013-12-12 13:02:52 +0800

    Fix porting problem: 1. GetCurTime() 2. frandom

--- Classes/Defines.h
+++ Classes/Defines.h
@@ -11,9 +11,9 @@
 #ifndef UINT64_C
 #define UINT64_C(val) val##ui64
 #endif
-#define random_range(low, high) (rand() % (high - low + 1)) + low
-#define frandom (float)rand() / UINT64_C(0x100000000)
-#define frandom_range(low, high) ((high - low) * frandom) + low
+#define random_range(low, high) ((rand() % (high - low + 1)) + low)
+#define frandom ((float)rand() / (RAND_MAX))
+#define frandom_range(low, high) (((high - low) * frandom) + low)
 
 // 3 - enumerations
 typedef enum _ActionState {
@@ -36,5 +36,6 @@ inline float GetCurTime(){
 // 	gettimeofday(&time, NULL);
 // 	unsigned long millisecs = (time.tv_sec * 1000) + (time.tv_usec / 1000);
 // 	return (float)millisecs;
-	return time(0) * 1000.0f;
+	//return time(0) * 1000.0f;
+	return clock() / ((float)CLOCKS_PER_SEC / 1000.0f);
 };
